7bac385b0a6aa097fcf1339b321ebaba28db93bf
tozastation <b1016031@fun.ac.jp>
2018-11-23 21:55:17 +0900

    #3 fix code format

--- myshell1016031.go
+++ myshell1016031.go
@@ -25,76 +25,38 @@ func main() {
 		if arr[0] == "bye" {
 			os.Exit(0)
 		}
-		if check_flag(arr) {
-			s, flagPoint := search_flag(arr)
-			s2, bitPoint := search_bit(arr, flagPoint)
+		// 区切り文字あるか確認
+		if checkFlag(arr) {
+			// ?より手前の切り出し
+			s, flagPoint := searchFlag(arr)
+			// ?より後の切り分け
+			s2, bitPoint := searchBit(arr, flagPoint)
 			args1 := s
 			args2 := s2
 			args3 := arr[bitPoint+1:]
-			attr := syscall.ProcAttr{Files: []uintptr{0, 1, 2}}
-			cpath, err := exec.LookPath(args1[0])
+			// とりあえず最初のコマンド実行
+			result, err := doCommand(args1[0], args1)
 			if err != nil {
 				log.Println(err)
+			}
+			// Exit出ちゃった時
+			if !result {
+				_, err := doCommand(args3[0], args3)
+				if err != nil {
+					log.Println(err)
+				}
+				// Successした時
 			} else {
-				pid, err := syscall.ForkExec(cpath, args1, &attr)
+				_, err := doCommand(args2[0], args2)
 				if err != nil {
 					log.Println(err)
-				}
-				proc, err := os.FindProcess(pid)
-				status, err := proc.Wait()
-				if err != nil {
-					log.Println(err)
-				}
-				if !status.Success() {
-					fmt.Println(status.String())
-					cpath, err := exec.LookPath(args3[0])
-					if err != nil {
-						log.Println(err)
-					}
-					pid, err := syscall.ForkExec(cpath, args3, &attr)
-					proc, err := os.FindProcess(pid)
-					status, err := proc.Wait()
-					if err != nil {
-						log.Println(err)
-					}
-					if !status.Success() {
-						fmt.Println(status.String())
-					}
-				} else {
-					cpath, err := exec.LookPath(args2[0])
-					if err != nil {
-						log.Println(err)
-					}
-					pid, err := syscall.ForkExec(cpath, args2, &attr)
-					proc, err := os.FindProcess(pid)
-					status, err := proc.Wait()
-					if err != nil {
-						log.Println(err)
-					}
-					if !status.Success() {
-						fmt.Println(status.String())
-					}
 				}
 			}
 		} else {
 			args := arr[0:]
-			attr := syscall.ProcAttr{Files: []uintptr{0, 1, 2}}
-			cpath, err := exec.LookPath(args[0])
+			_, err := doCommand(args[0], args)
 			if err != nil {
 				log.Println(err)
-			} else {
-				pid, err := syscall.ForkExec(cpath, args, &attr)
-				if err != nil {
-					log.Println(err)
-				}
-				proc, err := os.FindProcess(pid)
-				status, err := proc.Wait()
-				if err != nil {
-					log.Println(err)
-				}
-				if !status.Success() {
-					fmt.Println(status.String())
-				}
 			}
 		}
 		count++
@@ -102,7 +64,32 @@ func main() {
 	}
 }
 
-func search_flag(s []string) (s2 []string, i int) {
+func doCommand(topCommand string, option []string) (result bool, err error) {
+	attr := syscall.ProcAttr{Files: []uintptr{0, 1, 2}}
+	cpath, err := exec.LookPath(topCommand)
+	if err != nil {
+		return
+	}
+	pid, err := syscall.ForkExec(cpath, option, &attr)
+	if err != nil {
+		return
+	}
+	proc, err := os.FindProcess(pid)
+	if err != nil {
+		return
+	}
+	status, err := proc.Wait()
+	if err != nil {
+		return
+	}
+	if !status.Success() {
+		fmt.Println(status.String())
+	}
+	result = status.Success()
+	return
+}
+
+func searchFlag(s []string) (s2 []string, i int) {
 	for _, a := range s {
 		if a == flag {
 			break
@@ -114,7 +101,7 @@ func search_flag(s []string) (s2 []string, i int) {
 	return
 }
 
-func search_bit(s []string, count int) (s2 []string, i int) {
+func searchBit(s []string, count int) (s2 []string, i int) {
 	for _, a := range s {
 		if i > count {
 			if a == bit {
@@ -128,7 +115,7 @@ func search_bit(s []string, count int) (s2 []string, i int) {
 	return
 }
 
-func check_flag(s []string) (b bool) {
+func checkFlag(s []string) (b bool) {
 	b = false
 	for _, a := range s {
 		if a == flag {
